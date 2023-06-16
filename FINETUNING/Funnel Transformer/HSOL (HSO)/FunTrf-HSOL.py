6554ecf9f81902cd4ed2e49742373159ab21960e
Daniframe <66375746+Daniframe@users.noreply.github.com>
2023-06-16 18:53:55 +0200

    Update FunTrf-HSOL.py

--- FINETUNING/Funnel Transformer/HSOL (HSO)/FunTrf-HSOL.py
+++ FINETUNING/Funnel Transformer/HSOL (HSO)/FunTrf-HSOL.py
@@ -1,21 +1,12 @@
 import os
 import sys
 
-# Using only one GPU, they are being friendly enough
-# to let me use the server, don't get greedy :D
+# Using only one GPU to avoid server congestion
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-# To find my library
-sys.path.append("/home/daniroalv/miniconda3/envs/CodigoTFG/CodigoTFG")
-
-remote_username = "daniroalv"
-remote_host = "falco.dsic.upv.es"
-current_dir = os.getcwd()
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, TrainerCallback
 from sklearn.metrics import f1_score, accuracy_score
 from datasets import load_dataset
-import Transformations as Trf2
 from itertools import product
 from copy import deepcopy
 import numpy as np
@@ -70,10 +61,10 @@ first_val = True
 
 for n_ep, ini_lr in product(n_epochs, ini_learning_rate):
 
-    # Model: DistilBERT with a classification head of 3 classes
+    # Model: Funnel Transformer with a classification head of 3 classes
     model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels = 3)
 
-    # Computation of metrics: f1 score to deal with imbalanced datasts
+    # Computation of metrics: accuracy and f1 score to deal with imbalanced datasts
     def compute_metrics(pred):
         labels = pred.label_ids
         preds = pred.predictions.argmax(-1)
@@ -106,7 +97,10 @@ for n_ep, ini_lr in product(n_epochs, ini_learning_rate):
         compute_metrics = compute_metrics
     )
 
-    trainer.add_callback(CustomCallback(trainer))
+    # Uncomment the following line if you want to compute the metrics
+    # for the training dataset each epoch
+
+    # trainer.add_callback(CustomCallback(trainer))
     trainer.train()
 
     preds_train = trainer.predict(hso_train)
@@ -118,9 +112,11 @@ for n_ep, ini_lr in product(n_epochs, ini_learning_rate):
     train_f1 = preds_train.metrics["test_f1"]
     val_f1 = preds_val.metrics["test_f1"]
 
+    # Custom score: 20% f1-score train, 80% f1-score validation
     score = 0.2 * train_f1 + 0.8 * val_f1
     cad_scores += f"{ini_lr};{n_ep};{score}\n"
 
+    # Adhering to good practices in ML: providing results to avoid recomputation
     with open(rf"./FunTrf-HSOL_lr-{ini_lr}_nep-{n_ep}_train.csv", "w", encoding = "utf-8") as f:
 
         if first_train:

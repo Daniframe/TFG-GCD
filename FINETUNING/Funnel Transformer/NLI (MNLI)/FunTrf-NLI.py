6f04e7c6eddf766dbe3329b26c915d2aea386e7f
Daniframe <66375746+Daniframe@users.noreply.github.com>
2023-06-16 18:58:27 +0200

    Update FunTrf-NLI.py

--- FINETUNING/Funnel Transformer/NLI (MNLI)/FunTrf-NLI.py
+++ FINETUNING/Funnel Transformer/NLI (MNLI)/FunTrf-NLI.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-# Using only one GPU, they are being friendly enough
-# to let me use the server, don't get greedy :D
+# Using only one GPU to avoid server congestion
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-# To find my library
+# Path to the Transformations library. Change it to where Transformations.py
+# is located or get the library file in the same working directory as this file
 sys.path.append("/home/daniroalv/miniconda3/envs/CodigoTFG/CodigoTFG")
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, TrainerCallback
@@ -14,7 +14,6 @@ from datasets import load_dataset
 import Transformations as Trf2
 from itertools import product
 from copy import deepcopy
-import numpy as np
 import torch
 
 # Custom callback to also obtain the metrics of the training dataset
@@ -45,7 +44,7 @@ tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
 # Function to use in map: tokenization
 def tok_map(sample):
-    return tokenizer(sample["premise"], sample["hypothesis"], truncation = True, padding = "max_length")#, max_length = 96)
+    return tokenizer(sample["premise"], sample["hypothesis"], truncation = True, padding = "max_length")
 
 # Tokenization and preparation of dataset to the trainer
 mnli_train = mnli_train_temp.map(tok_map, batched = True)
@@ -65,10 +64,10 @@ first_val = True
 
 for n_ep, ini_lr in product(n_epochs, ini_learning_rate):
 
-    # Model: DistilBERT with a classification head of 2 classes
+    # Model: Funnel transformer with a classification head of 2 classes
     model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels = 3)
 
-    # Computation of metrics: f1 score to deal with imbalanced datasts
+    # Computation of metrics: accuracy and f1 score to deal with imbalanced datasts
     def compute_metrics(pred):
         labels = pred.label_ids
         preds = pred.predictions.argmax(-1)
@@ -101,7 +100,10 @@ for n_ep, ini_lr in product(n_epochs, ini_learning_rate):
         compute_metrics = compute_metrics
     )
 
-    # trainer.add_callback(CustomCallback(trainer))
+    # Uncomment the following line if you want to compute the metrics
+    # for the training dataset each epoch
+
+    # trainer.add_callback(CustomCallback(trainer)
     trainer.train()
 
     preds_train = trainer.predict(mnli_train)
@@ -113,9 +115,11 @@ for n_ep, ini_lr in product(n_epochs, ini_learning_rate):
     train_f1 = preds_train.metrics["test_f1"]
     val_f1 = preds_val.metrics["test_f1"]
 
+    # Custom score: 20% f1-score train, 80% f1-score validation
     score = 0.2 * train_f1 + 0.8 * val_f1
     cad_scores += f"{ini_lr};{n_ep};{score}\n"
 
+    # Adhering to good practices in ML: providing results to avoid recomputation
     with open(rf"./FunTrf-NLI_lr-{ini_lr}_nep-{n_ep}_train.csv", "w", encoding = "utf-8") as f:
 
         if first_train:
